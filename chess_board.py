8d6157781836cdf7b807f9ce05997ca28440b10b
Timelord65 <praneethchokka@gmail.com>
2023-02-27 03:45:10 +0530

    Added functions for displaying chess pieces and some design decisions.

--- chess_board.py
+++ chess_board.py
@@ -16,6 +16,94 @@ blue = (0, 0, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+P_assets = "./assets"
+
+def disp_Blackking(x, y):
+    piece_name = "black king"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Blackqueen(x, y):
+    piece_name = "black queen"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Blackbishop(x, y):
+    piece_name = "black bishop"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+
+def disp_Blackknight(x, y):
+    piece_name = "black knight"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Blackrook(x, y):
+    piece_name = "black rook"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Blackpawn(x, y):
+    piece_name = "black pawn"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+
+def disp_Whiteking(x, y):
+    piece_name = "white king"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Whitequeen(x, y):
+    piece_name = "white queen"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Whitebishop(x, y):
+    piece_name = "white bishop"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+
+def disp_Whiteknight(x, y):
+    piece_name = "white knight"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Whiterook(x, y):
+    piece_name = "white rook"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
+
+def disp_Whitepawn(x, y):
+    piece_name = "white pawn"
+    path = P_assets + "/Pieces/" + piece_name + ".png"
+    img = pygame.image.load(path)
+    gameDisplay.blit(img, (x, y))
+    return
 
 
 gameDisplay = pygame.display.set_mode((dw, dh))
@@ -29,6 +117,7 @@ while running:
             running = False
         
     gameDisplay.fill(white)
+    disp_Whiteknight(200, 200)
 
     pygame.display.update()
 
