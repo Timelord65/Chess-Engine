63042e92600666052e7b19f878375340efbfa0c7
Timelord65 <praneethchokka@gmail.com>
2023-03-04 06:14:23 +0530

    Board background is being rendered successfully

--- chess_board.py
+++ chess_board.py
@@ -4,6 +4,7 @@ from copy import deepcopy
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
 import pygame
 import sys
+import chess
 
 pygame.init()
 
@@ -11,7 +12,7 @@ dw = 800    #Stands for display width
 dh = 600    #Stands for display height
 
 red = (255, 0, 0)
-green = (0, 255, 0)
+green = (1, 50, 32)
 blue = (0, 0, 255)
 black = (0, 0, 0)
 white = (255, 255, 255)
@@ -105,7 +106,36 @@ def disp_Whitepawn(x, y):
     gameDisplay.blit(img, (x, y))
     return
 
+def drawBackground(): #Function to draw the background chess board. 
+    boundaryRect = pygame.Rect((200, 100), (400, 400))
+    pygame.draw.rect(gameDisplay, pygame.Color(green), boundaryRect)
+    prev_color = white
 
+    for i in range(0, 8):
+        if prev_color == white:
+            prev_color = green
+        else:
+            prev_color = white
+        for j in range(0, 8):
+
+            if prev_color == white:
+                    current = green
+                    prev_color = green
+            
+            else:
+                current = white
+                prev_color = white
+
+            tile = pygame.Rect((200 + (50*i), 100 + (50*j)), (50, 50))
+            pygame.draw.rect(gameDisplay, pygame.Color(current), tile)
+            
+            #print("Current i: %d, Current j: %d, Current: %s, prev: %s" %(i, j, current, prev_color))
+
+def drawBoard(board): #Function to display the pychess board on the GUI. 
+    pass
+
+
+    
 gameDisplay = pygame.display.set_mode((dw, dh))
 pygame.display.set_caption('Chess Game')
 
@@ -116,7 +146,8 @@ while running:
         if event.type == pygame.QUIT:
             running = False
         
-    gameDisplay.fill(white)
+    drawBackground()
+    #gameDisplay.fill(white)
     disp_Whiteknight(200, 200)
 
     pygame.display.update()
