6c8b5b8bcf7ecb0cdf5bcd3ea420027afe3cfd00
Timelord65 <praneethchokka@gmail.com>
2023-08-15 16:10:00 +0530

    Changed chess_board.py to not use gamedisplay as global variable

--- chess_board.py
+++ chess_board.py
@@ -1,16 +1,16 @@
+import chess
+import sys
+import pygame
 from time import sleep
 import os
 from copy import deepcopy
 os.environ['PYGAME_HIDE_SUPPORT_PROMPT'] = "hide"
-import pygame
-import sys
-import chess
 
 board = chess.Board()
 pygame.init()
 
-dw = 800    #Stands for display width
-dh = 600    #Stands for display height
+dw = 800  # Stands for display width
+dh = 600  # Stands for display height
 
 red = (255, 0, 0)
 green = (1, 50, 32)
@@ -20,21 +20,24 @@ white = (255, 255, 255)
 
 P_assets = "./assets"
 
-def disp_Blackking(x, y):
+
+def disp_Blackking(x, y, gameDisplay):
     piece_name = "black king"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Blackqueen(x, y):
+
+def disp_Blackqueen(x, y, gameDisplay):
     piece_name = "black queen"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Blackbishop(x, y):
+
+def disp_Blackbishop(x, y, gameDisplay):
     piece_name = "black bishop"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
@@ -42,21 +45,23 @@ def disp_Blackbishop(x, y):
     return
 
 
-def disp_Blackknight(x, y):
+def disp_Blackknight(x, y, gameDisplay):
     piece_name = "black knight"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Blackrook(x, y):
+
+def disp_Blackrook(x, y, gameDisplay):
     piece_name = "black rook"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Blackpawn(x, y):
+
+def disp_Blackpawn(x, y, gameDisplay):
     piece_name = "black pawn"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
@@ -64,21 +69,23 @@ def disp_Blackpawn(x, y):
     return
 
 
-def disp_Whiteking(x, y):
+def disp_Whiteking(x, y, gameDisplay):
     piece_name = "white king"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Whitequeen(x, y):
+
+def disp_Whitequeen(x, y, gameDisplay):
     piece_name = "white queen"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Whitebishop(x, y):
+
+def disp_Whitebishop(x, y, gameDisplay):
     piece_name = "white bishop"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
@@ -86,28 +93,32 @@ def disp_Whitebishop(x, y):
     return
 
 
-def disp_Whiteknight(x, y):
+def disp_Whiteknight(x, y, gameDisplay):
     piece_name = "white knight"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Whiterook(x, y):
+
+def disp_Whiterook(x, y, gameDisplay):
     piece_name = "white rook"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def disp_Whitepawn(x, y):
+
+def disp_Whitepawn(x, y, gameDisplay):
     piece_name = "white pawn"
     path = P_assets + "/Pieces/" + piece_name + ".png"
     img = pygame.image.load(path)
     gameDisplay.blit(img, (x, y))
     return
 
-def drawBackground(): #Function to draw the background chess board. 
+
+# Function to draw the background chess board.
+def drawBackground(gameDisplay):
     boundaryRect = pygame.Rect((200, 100), (400, 400))
     pygame.draw.rect(gameDisplay, pygame.Color(green), boundaryRect)
     prev_color = white
@@ -120,19 +131,20 @@ def drawBackground(): #Function to draw the background chess board.
         for j in range(0, 8):
 
             if prev_color == white:
-                    current = green
-                    prev_color = green
-            
+                current = green
+                prev_color = green
+
             else:
                 current = white
                 prev_color = white
 
             tile = pygame.Rect((200 + (50*i), 100 + (50*j)), (50, 50))
             pygame.draw.rect(gameDisplay, pygame.Color(current), tile)
-            
-            #print("Current i: %d, Current j: %d, Current: %s, prev: %s" %(i, j, current, prev_color))
 
-def drawBoard(board): #Function to display the pychess board on the GUI. Translate the fenstring from pychess board to positions on the chess_board
+            # print("Current i: %d, Current j: %d, Current: %s, prev: %s" %(i, j, current, prev_color))
+
+
+def drawBoard(board, gameDisplay):  # Function to display the pychess board on the GUI. Translate the fenstring from pychess board to positions on the chess_board
     char_piece_map = {
         'k': disp_Blackking,
         'q': disp_Blackqueen,
@@ -147,9 +159,9 @@ def drawBoard(board): #Function to display the pychess board on the GUI. Transla
         'R': disp_Whiterook,
         'P': disp_Whitepawn
     }
-    
-    #tile = 0
-    #fenString = board.fen()
+
+    # tile = 0
+    # fenString = board.fen()
     fenString = "r1bkr/p2pBpNp/n4n2/1p1NP2P/6P1/3P4/P1P1K3/q5b1"
     fen = fenString.split(' ')[0]
     for i in range(8):
@@ -158,27 +170,31 @@ def drawBoard(board): #Function to display the pychess board on the GUI. Transla
         for c in row:
 
             if c.isalpha():
-                char_piece_map[c](200 + 50*j, 100 + 50*i)
+                char_piece_map[c](200 + 50*j, 100 + 50*i, gameDisplay)
                 j += 1
             else:
                 j += ord(c) - 48
 
-    
-gameDisplay = pygame.display.set_mode((dw, dh))
-pygame.display.set_caption('Chess Game')
 
-running = True
+def main():
+    gameDisplay = pygame.display.set_mode((dw, dh))
+    pygame.display.set_caption('Chess Game')
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        
-    drawBackground()
-    drawBoard(board)
-    #gameDisplay.fill(white)
+    running = True
 
-    pygame.display.update()
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
 
-pygame.quit()
-quit()
+        drawBackground(gameDisplay)
+        drawBoard(board, gameDisplay)
+        # gameDisplay.fill(white)
+
+        pygame.display.update()
+
+    pygame.quit()
+    quit()
+
+
+main()
